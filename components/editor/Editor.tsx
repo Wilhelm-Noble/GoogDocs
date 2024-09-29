7cfcd07a135bc37cf141108f3f1cd471a23c959e
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-09-29 14:08:27 +0800

    test

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -22,6 +22,7 @@ import { DeleteModal } from '../DeleteModal';
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
+//sample
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
