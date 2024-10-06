648508769386f5bd3bd1b3132fed1fa97599ece9
Wilhelm Noble <100698823+Wilhelm-Noble@users.noreply.github.com>
2024-10-06 21:37:14 +0800

    prettier

--- components/ui/CollaborativeRoom.tsx
+++ components/ui/CollaborativeRoom.tsx
@@ -1,33 +1,32 @@
-'use client';
+"use client";
 
-import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
-import React from 'react'
-import Editor from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs'
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import React from "react";
+import Editor from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
-const CollaborativeRoom = (children:{children:ReactNode}) => {
+const CollaborativeRoom = (children: { children: ReactNode }) => {
   return (
     <RoomProvider id="my-room">
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
-        <Header>
-        <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'>Title</p>
-
-        </div>
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor />
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Title</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
-  )
-}
+  );
+};
 
-export default CollaborativeRoom
+export default CollaborativeRoom;
